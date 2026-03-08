3853e1a3a2c8ac8b2a21cefb8e6684643ec6e2c3
LeVion16 <nemeth.levente.vbj@gmail.com>
2026-03-08 19:40:05 +0100

    javítás

--- test/2026.03.02/a/megoldas.py
+++ test/2026.03.02/a/megoldas.py
@@ -1,76 +1,75 @@
-# A program kérjen be a felhasználótól egysornyi, csillaggal tagolt adatot...
-
-data = input("Adja meg az adatait csillag-karakterrel tagolva: ")
-data_list = data.split("*")
-data_end = data_list[-1].replace(".","-")
-
-output = f"{data_list[0]};{data_list[1]};{data_list[2]};{data_list[3].replace(".","-")}"
-# print(output)
-
-with open("./user.txt", "w", encoding="utf-8") as f:
-    print( output, file=f)
-    
-# Fájlbeolvasás:
-    
-path = "./orvosi_nobeldijak.csv"
+path = "./fuvar.csv"
 f_content = list()
 with open(path, "r", encoding="utf-8") as f:
     raw = f.readlines()
-
+    
 header = raw[0].strip().split(";")
-
 for i in range(1, len(raw), 1):
-    line = raw[i].strip().split(";")
     d = dict()
-    d["Év"] = int(line[0])
-    d["Név"] = line[1]
-    d["SzületésHalálozás"] = line[2]
-    d["Országkód"] = line[3]
+    line = raw[i].strip().split(";")
+    d["taxi_id"] = int(line[0])
+    d["indulas"] = line[1]
+    d["idotartam"] = int(line[2])
+    d["tavolsag"] = float(line[3].replace(",", "."))
+    d["viteldij"] = float(line[4].replace(",", "."))
+    d["borravalo"] = float(line[5].replace(",", "."))
+    d["fizetes_modja"] = line[6]
+    
     f_content.append(d)
     
 # print(f_content)
 
-# Határozza meg, és írja ki a képernyőre, hogy hány, ma már nem élő díjazott adatai találhatóak a forrásállományban!
-def dead_nobels(array: list) -> int:
+# 6185-ös azonosítójú taxis bevétele, fuvarjainak száma:
+def taxi_data(array: list, id: int) -> str:
+    summa = 0
     count = 0
     for line in array:
-        if len(line["SzületésHalálozás"]) == 5:
+        if line["taxi_id"] == id:
+            summa += (line["viteldij"] + line["borravalo"])
+            count += 1
+    return f"A(z) {id} azonosítójú taxis bevétele: {summa} $, fuvarjainak száma: {count} db"
+print(f"\n{taxi_data(f_content, 6185)}")
+
+# összesen hány km-t tettek meg a taxisok (1 mérföld : 1,6 km) -- két tizedesjegy:
+def summa_km(array: list) -> float:
+    summa = 0
+    for line in array:
+        summa += line["tavolsag"]
+    return round(summa * 1.6, 2)
+
+print(f"\n Összesen {summa_km(f_content)} km-t tettek meg a taxisok.")
+
+# Határozza meg, és írja ki a képernyőre, a minta szerint, hogy 15-én hányan fizettek bankkártyával!
+
+def count_paying_instrument(array: list, instrument: str) -> int:
+    count = 0
+    for line in array:
+        if line["fizetes_modja"] == instrument and line["indulas"][8:10:1] == '15':
             count += 1
     return count
 
-print(f"\nMa már nem élő díjazottak száma: {dead_nobels(f_content)} fő.")
+print(f"\nBankkártyával {count_paying_instrument(f_content, "bankkártya")} utas fizetett.")
 
-# Határozza meg és írja a képernyőre, hogy az adatforrásban melyik volt az utolsó (legnagyobb) díjátadó éve, amelyben a díjazott(ak) adatait rögzítették!
-
-def last_year(array: list) -> int:
-    last = 0
+# írja ki a large.txt fájlba, a minta szerint, az időben leghosszabb fuvar adatait!
+def large_transport(array: list) -> str:
+    maximum = 0
+    current_line = None
     for line in array:
-        if last < line["Év"]:
-            last = line["Év"]
-    return last
+        if line["tavolsag"] >  maximum:
+            maximum = line["tavolsag"]
+            current_line = line
+    return f"{current_line["taxi_id"]};{current_line["indulas"]};{current_line["idotartam"]};{current_line["tavolsag"]};{current_line["viteldij"]};{current_line["borravalo"]};{current_line["fizetes_modja"]}"
 
-print(f"\nUtolsó év: {last_year(f_content)}")
+with open("./large.txt", "w", encoding="utf-8") as f:
+    print(large_transport(f_content), file=f)
+
+# Határozza meg, és írja ki a képernyőre, a minta szerint, hogy volt-e olyan eset, amikor a megtett távolság nulla mérföld. A keresést csak addig folytassa, amíg nem talál ilyen esetet!
+
+k = 0
+while k < len(f_content) and f_content[k]["tavolsag"] != 0:
+    k += 1
     
-# Kérje be a felhasználótól egy ország kódját! Ha a megadott országból nem volt díjazott, akkor „A megadott országból nem volt díjazott!” szöveget írja ki, ha az országból csak egy díjazott volt, akkor jelenítse meg az adatait! Ha a keresett országból több orvosi Nobel-díjas is volt az évek során, akkor írja ki a számukat a következő minta szerint: „A megadott országból 3 fő díjazott volt!”.
-
-code = input("\nKérem, adja meg egy ország kódját: ")
-
-def count_code(array: list, code: str) -> int:
-    count = 0
-    for line in  array:
-        if line["Országkód"] == code:
-            count += 1
-    return count
-
-if count_code(f_content, code) > 1:
-    print(f"\nA megadott országból {count_code(f_content, code)} fő díjazott volt.")
-elif count_code(f_content, code) == 1:
-    print(f"\tA megadott ország díjazottja: ")
-    k = 0
-    while f_content[k]["Országkód"] != code:
-        k += 1
-    print(f"\tNév: {f_content[k]["Név"]}")
-    print(f"\tÉv: {f_content[k]["Év"]}")
-    print(f"\tSz/H: {f_content[k]["SzületésHalálozás"]}")
+if k < len(f_content):
+    print("\nVolt olyan eset, amikor a megtett távolsá 0 mérföld volt.\n")
 else:
-    print("\nA megadott országból nem volt díjazott.")
+    print("\nNem volt olyan eset, amikor a megtett távolsá 0 mérföld volt.\n")
